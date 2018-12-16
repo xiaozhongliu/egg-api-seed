f9c28933d8a5a7b66aaf8efb6fe3958c551a10d6
xiaozhongliu <xiaozhong.liu@outlook.com>
2018-12-16 13:13:09 +0800

    logic generating controllers & services

--- app/service/Test.ts
+++ app/service/Test.ts
@@ -2,10 +2,6 @@ import { Service } from 'egg'
 
 export default class Test extends Service {
 
-    /**
-     * say hi to you
-     * @param name - your name
-     */
     public async sayHi(name: string) {
         return `hi, ${name}`
     }
--- codegen/index.ts
+++ codegen/index.ts
@@ -59,13 +59,14 @@ function deserialize(content: string): Package {
             continue
         }
         if (isParsingService && /rpc\s+/.test(line)) {
+            // @ts-ignore
             const [str, name, req, res] = /rpc\s+(.*?)\s+\((.*?)\)\s+returns\s+\((.*?)\)\s+{}/.exec(line)!
 
             let reqMessage
             let resMessage
             if (pack.messages.length) {
-                reqMessage = pack.messages.find(((message) => req === message.name))
-                resMessage = pack.messages.find(((message) => res === message.name))
+                reqMessage = pack.messages.find(message => req === message.name)
+                resMessage = pack.messages.find(message => res === message.name)
             }
             if (!reqMessage) {
                 reqMessage = new Message(req)
@@ -93,7 +94,7 @@ function deserialize(content: string): Package {
         if (!isParsingMessage && line.startsWith('message')) {
             isParsingMessage = true
             const name = /message\s+(.*?)\s+{/.exec(line)![1]
-            currentMessage = pack.messages.find(((message) => name === message.name))
+            currentMessage = pack.messages.find(message => name === message.name)
 
             if (!pack) {
                 throw new Error(`proto error: unused message [${name}]`)
@@ -102,6 +103,7 @@ function deserialize(content: string): Package {
             continue
         }
         if (isParsingMessage && line !== '}') {
+            // @ts-ignore
             const [str, type, name, binaryId] = /\s+(.*?)\s+(.*?)\s+=\s+(.*?);/.exec(line)!
             const property = new Property(name, type, parseInt(binaryId))
             currentMessage.properties.push(property)
@@ -125,72 +127,79 @@ function deserialize(content: string): Package {
 function generate(pack: Package) {
     console.log('\nfinal AST: ', JSON.stringify(pack, null, '    '))
 
-    const packDir = `${__dirname}/__${pack.name}`
-    shell.exec(`rm -rf ${packDir}`)
-    shell.exec(`mkdir ${packDir}`)
+    const controllerDir = `${__dirname}/dist/controller`
+    const serviceDir = `${__dirname}/dist/service`
     const typeDir = `type/${pack.name}`
-    shell.exec(`rm -rf ${typeDir}`)
-    shell.exec(`mkdir ${typeDir}`)
+    recreateFolder(controllerDir, serviceDir, typeDir)
 
     /**
-     * generate ctrl
+     * generate controller and service
      */
-    pack.services.forEach((service) => {
-        const fileName = `${service.name.toLowerCase()}-ctrl.ts`
-        const stream = fs.createWriteStream(`${packDir}/${fileName}`)
+    pack.services.forEach(service => {
+        const fileName = `${service.name.toLowerCase()}.ts`
+        let stream = fs.createWriteStream(`${controllerDir}/${fileName}`)
 
-        const imports = service.methods.map(({ response }) => `import ${response.name} from '../type/${pack.name}/${response.name}'`)
-        const distinctImports = [...new Set(imports)]
-        stream.write(`${distinctImports.join('\n')}\n
-export default {\n
-    // proto package name
-    package: '${pack.name}',
-    // proto service name
-    service: '${service.name}',`,
-        )
+        stream.write(`import { Controller } from 'egg'\n
+export default class ${service.name}Controller extends Controller {`)
 
         for (const method of service.methods) {
             stream.write(`\n
-    async ${ method.name}(req: ${method.request.name}) {
-        return new ${method.response.name}(
-            ${ method.response.properties.map(() => 'undefined,').join('\n            ')}
-        )
-    },`,
-            )
+    public async ${ method.name}() {
+        const { ctx } = this
+        ctx.body = await ctx.service.${service.name.toLowerCase()}.${method.name}(${method.response.properties.map(() => 'null').join(',')})
+    }`)
         }
 
         stream.write('\n}\n')
         stream.end()
-        console.log('\ngenerated ctrl:', fileName)
+        console.log('\ngenerated controller:', fileName)
+
+        // ---
+
+        stream = fs.createWriteStream(`${serviceDir}/${fileName}`)
+
+        stream.write(`import { Service } from 'egg'\n
+export default class ${service.name} extends Service {`)
+
+        for (const method of service.methods) {
+            stream.write(`\n
+    public async ${ method.name}(${method.response.properties.map(property => `${property.name}: ${property.type}`).join(',')}) {
+        return { ${method.response.properties.map(property => property.name).join(', ')} }
+    }`)
+        }
+
+        stream.write('\n}\n')
+        stream.end()
+        console.log('generated service:', fileName)
     })
 
     /**
      * generate types
      */
-    pack.messages.forEach((message) => {
-        const isRequestType = message.name.includes('Request')
-        const fileName = `${message.name}${isRequestType ? '.d' : ''}.ts`
-        const stream = fs.createWriteStream(`${typeDir}/${fileName}`)
+    //     pack.messages.forEach(message => {
+    //         const isRequestType = message.name.includes('Request')
+    //         const fileName = `${message.name}${isRequestType ? '.d' : ''}.ts`
+    //         const stream = fs.createWriteStream(`${typeDir}/${fileName}`)
 
-        if (isRequestType) {
-            stream.write(`interface ${message.name} extends Req {\n
-    body: {${ message.properties.map((property) => `\n        ${property.name}: ${property.type}`).join()}
-    }
-}\n`,
-            )
-        } else {
-            stream.write(`export default class ${message.name} {\n
-    constructor(${ message.properties.map((property) => `${property.name}: ${property.type}`).join(', ')}) {
-${ message.properties.map((property) => `        this.${property.name} = ${property.name}`).join('\n')}
-    }
-${ message.properties.map((property) => `\n    ${property.name}: ${property.type}`).join()}
-}\n`,
-            )
-        }
+    //         if (isRequestType) {
+    //             stream.write(`interface ${message.name} extends Req {\n
+    //     body: {${ message.properties.map(property => `\n        ${property.name}: ${property.type}`).join()}
+    //     }
+    // }\n`,
+    //             )
+    //         } else {
+    //             stream.write(`export default class ${message.name} {\n
+    //     constructor(${ message.properties.map(property => `${property.name}: ${property.type}`).join(', ')}) {
+    // ${ message.properties.map(property => `        this.${property.name} = ${property.name}`).join('\n')}
+    //     }
+    // ${ message.properties.map(property => `\n    ${property.name}: ${property.type}`).join()}
+    // }\n`,
+    //             )
+    //         }
 
-        stream.end()
-        console.log('generated %s: %s', isRequestType ? 'type' : 'class', fileName)
-    })
+    //         stream.end()
+    //         console.log('generated %s: %s', isRequestType ? 'type' : 'class', fileName)
+    //     })
 }
 
 /**
@@ -204,11 +213,18 @@ function getOnlineProtoFile(filename: string) {
 function upodateMethodsMessages(pack: Package) {
     for (const { methods } of pack.services) {
         for (const method of methods) {
-            const reqMessage = pack.messages.find((message) => method.request.name === message.name)
-            const resMessage = pack.messages.find((message) => method.response.name === message.name)
+            const reqMessage = pack.messages.find(message => method.request.name === message.name)
+            const resMessage = pack.messages.find(message => method.response.name === message.name)
             method.request = reqMessage as Message
             method.response = resMessage as Message
         }
     }
     return pack
+}
+
+function recreateFolder(...dirs: string[]) {
+    dirs.forEach(dir => {
+        shell.exec(`rm -rf ${dir}`)
+        shell.exec(`mkdir -p ${dir}`)
+    })
 }
