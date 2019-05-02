9d8a020667c15acf76ce02625111cdcb83a64a76
xiaozhongliu <xiaozhong.liu@outlook.com>
2019-05-02 10:40:59 +0800

    router middleware: validate

--- app/middleware/validate.ts
+++ app/middleware/validate.ts
@@ -1,9 +1,9 @@
-import { Application, Context } from 'egg'
+import { Context } from 'egg'
 
-module.exports = (options: any, app: Application) => {
-    return async (ctx: Context, next: Function) => {
-        console.log('enter midware')
-        await next()
-        console.log('exit midware')
-    }
+export default async (ctx: Context, next: Function) => {
+    console.log('enter midware')
+    console.log(JSON.stringify(ctx))
+    await next()
+    console.log('exit midware')
+    console.log(JSON.stringify(ctx))
 }
--- app/router.ts
+++ app/router.ts
@@ -3,20 +3,22 @@ import { Application } from 'egg'
 export default (app: Application) => {
 
     let rp
+    const { validate } = app.middleware
     const { controller, router } = app
-    router.get('home.index', '/', controller.home.index)
-    router.get('home.validate', '/validate', controller.home.validate)
-    router.get('home.grpc', '/grpc', controller.home.grpc)
-    router.get('home.header', '/header', controller.home.header)
-    router.get('home.timeout', '/timeout', controller.home.timeout)
-    router.get('home.failover', '/failover', controller.home.failover)
-    router.get('home.pause', '/pause', controller.home.pause)
-    router.get('home.eatmem', '/eatmem', controller.home.eatmem)
-    router.get('home.getmem', '/getmem', controller.home.getmem)
-    router.get('home.eatcpu', '/eatcpu', controller.home.eatcpu)
-    router.get('home.getcpu', '/getcpu', controller.home.getcpu)
+
+    router.get('home.index', '/', validate, controller.home.index)
+    router.get('home.validate', '/validate', validate, controller.home.validate)
+    router.get('home.grpc', '/grpc', validate, controller.home.grpc)
+    router.get('home.header', '/header', validate, controller.home.header)
+    router.get('home.timeout', '/timeout', validate, controller.home.timeout)
+    router.get('home.failover', '/failover', validate, controller.home.failover)
+    router.get('home.pause', '/pause', validate, controller.home.pause)
+    router.get('home.eatmem', '/eatmem', validate, controller.home.eatmem)
+    router.get('home.getmem', '/getmem', validate, controller.home.getmem)
+    router.get('home.eatcpu', '/eatcpu', validate, controller.home.eatcpu)
+    router.get('home.getcpu', '/getcpu', validate, controller.home.getcpu)
 
     rp = app.router.namespace('/greeter')
-    rp.get('greeter.sayHello', '/sayHello', controller.greeter.sayHello)
-    rp.get('greeter.sayGoodbye', '/sayGoodbye', controller.greeter.sayGoodbye)
+    rp.get('greeter.sayHello', '/sayHello', validate, controller.greeter.sayHello)
+    rp.get('greeter.sayGoodbye', '/sayGoodbye', validate, controller.greeter.sayGoodbye)
 }
